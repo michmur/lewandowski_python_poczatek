c0f92594f60cd9c15e15c2ba151f0a05696aeaa2
Michal Murawski <77murawski@gmail.com>
2022-03-30 12:56:05 +0200

    Create konstruktor_i_pola_obiektu_4.py

---
+++ programowanie_obiektowe_1/konstruktor_i_pola_obiektu_4.py
@@ -0,0 +1,33 @@
+# Obiekt może zawierać w sobie inne obiekty
+class School():
+    def __init__(self, name, students) -> None:
+        self.name = name
+        self.students = students
+
+class Student():
+
+    # Konstruktor może przyjąć argumenty
+    def __init__(self, first_nane, last_name) -> None:
+        self.first_nane = first_nane
+        self.last_name = last_name
+        self.promoted = False
+
+# Obiekt możemy przekazywać jako argumenty do funkcji
+def print_student(student):
+    print(f"Student : {student.first_nane} {student.last_name}, promoted : {student.promoted}")
+
+def promoted_student(student):
+    student.promoted = True
+
+def run_example():
+    student = Student(first_nane="Ola", last_name="Nowak")
+    print_student(student)
+
+    other_student = Student("Jerzy","Jurkowski")
+    print_student(other_student)
+
+    promoted_student(student)
+    print_student(student)
+
+if __name__ == '__main__':
+    run_example()

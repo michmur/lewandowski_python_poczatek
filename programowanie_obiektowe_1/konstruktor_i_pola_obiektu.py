22561a10728dcfae06700439b482361d72c09417
Michal Murawski <77murawski@gmail.com>
2022-03-30 12:04:45 +0200

    Update konstruktor_i_pola_obiektu.py

--- programowanie_obiektowe_1/konstruktor_i_pola_obiektu.py
+++ programowanie_obiektowe_1/konstruktor_i_pola_obiektu.py
@@ -1,12 +1,26 @@
-from tokenize import String
-
-
 class Student():
 
     # Konstruktor zostanie wywołany podczas tworzenia obiektu
-    def __init__(self) -> None:
-        print("Powstaje nowy obiekt!")
+    # def __init__(self) -> None:
+    #     print("Powstaje nowy obiekt!")
 
+    # Zadaniem konstruktora jest zdefiniować i zainicjować stan obiketu
+    def __init__(self) -> None:
+        self.first_nane = "Mikołaj"
+        self.last_name = "Lewandowski"
+        self.age = 54
+
+
+def run_example():
+    student = Student()
+    # Do stanu obiektu mamy dostęp - możemy go odczytać
+    print(f"imie : {student.first_nane}, nazwisko : {student.last_name} wiek : {student.age}")
+    
+    # Stan obiektu możemy również zmodyfikować
+    student.first_nane = "Jakub"
+    student.last_name = "Kowalski"
+    student.age = 30
+    print(f"imie : {student.first_nane}, nazwisko : {student.last_name} wiek : {student.age}")
 
 if __name__ == '__main__':
-    student = Student()
+    run_example()

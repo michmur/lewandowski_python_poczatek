4dc161768ac2255ad7325bd68c5462b7f1a64513
Michal Murawski <77murawski@gmail.com>
2022-03-30 13:24:09 +0200

    Update konstruktor_i_pola_obiektu_4.py

--- programowanie_obiektowe_1/konstruktor_i_pola_obiektu_4.py
+++ programowanie_obiektowe_1/konstruktor_i_pola_obiektu_4.py
@@ -1,3 +1,5 @@
+import random
+
 # Obiekt może zawierać w sobie inne obiekty
 class School():
     def __init__(self, name, students) -> None:
@@ -7,27 +9,35 @@ class School():
 class Student():
 
     # Konstruktor może przyjąć argumenty
-    def __init__(self, first_nane, last_name) -> None:
-        self.first_nane = first_nane
+    def __init__(self, first_name, last_name) -> None:
+        self.first_name = first_name
         self.last_name = last_name
         self.promoted = False
 
 # Obiekt możemy przekazywać jako argumenty do funkcji
 def print_student(student):
-    print(f"Student : {student.first_nane} {student.last_name}, promoted : {student.promoted}")
+    print(f"Student : {student.first_name} {student.last_name}, promoted : {student.promoted}")
 
 def promoted_student(student):
     student.promoted = True
 
+# Funkcja może zwracać obiekty
+def creata_school_with_students(school_name):
+    numbers_of_students = random.randint(1,20)
+    students = []
+    for student_number in range(numbers_of_students):
+        first_name = f"Student-{student_number}"
+        last_name = "Smith"
+        students.append(Student(first_name,last_name))
+    
+    school = School(school_name,students)
+    return school
+
 def run_example():
-    student = Student(first_nane="Ola", last_name="Nowak")
-    print_student(student)
-
-    other_student = Student("Jerzy","Jurkowski")
-    print_student(other_student)
-
-    promoted_student(student)
-    print_student(student)
+    school = creata_school_with_students("Hogwart")
+    print(school)
+    for student in school.students:
+        print_student(student)
 
 if __name__ == '__main__':
     run_example()
